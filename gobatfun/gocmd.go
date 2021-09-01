51a7473fd351b1a5cadddba57554ce9c9452d2d3
juanpickselov <juanpickselov@sbcglobal.net>
2021-08-31 21:04:33 -0400

    added /C back

--- gobatfun/gocmd.go
+++ gobatfun/gocmd.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	cmd := exec.Command("cmd.exe", "/K", "start", "", "powershell.exe", "-NoExit", "-Command", "& {Get-TimeZone; Write-Host 'blah'; Read-Host 'Please hit enter to exit'; exit}")
+	cmd := exec.Command("cmd.exe", "/C", "start", "", "powershell.exe", "-NoExit", "-Command", "& {Get-TimeZone; Write-Host 'blah'; Read-Host 'Please hit enter to exit'; exit}")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stdout
 
