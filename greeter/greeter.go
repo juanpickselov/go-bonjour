8be4b97930e21567b34dd84e691c43f7b7439c75
juanpickselov <juanpickselov@sbcglobal.net>
2021-12-25 14:00:55 -0500

    tutorial wip amongst my messing around with GO

--- greeter/greeter.go
+++ greeter/greeter.go
@@ -15,6 +15,18 @@ func Greet(name string) (string, error) {
 	return message, nil
 }
 
+func Greets(names []string) (map[string]string,error) {
+	messages := make(map[string]string)
+	for _, name := range names {
+		message, err := Greet(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+	return messages, nil
+}
+
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }
