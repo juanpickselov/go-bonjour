dbf2b214baa40d423163cb86e076defeb1405d4a
juanpickselov <juanpickselov@sbcglobal.net>
2021-12-25 13:45:46 -0500

    tutorial wip amongst my messing around with GO

--- greetyo/yo.go
+++ greetyo/yo.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"juanpickselov/greeter"
+	"log"
 )
 
 func main() {
 	log.SetPrefix("greeter: ")
 	log.SetFlags(0)
 
-	message, err := greeter.Greet("Juan")
+	names := []string{"Juan", "Sofia", "Anastasia"}
+
+	message, err := greeter.Greets(names)
 	if err != nil {
 		log.Fatal(err)
 	}
